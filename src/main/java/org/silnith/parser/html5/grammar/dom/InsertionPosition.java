09652701cbe0512e29fe826ec6fce0952379511b
kent <silnith@gmail.com>
2016-04-28 20:11:23 -0700

    Fix docs for insertion position.

--- src/main/java/org/silnith/parser/html5/grammar/dom/InsertionPosition.java
+++ src/main/java/org/silnith/parser/html5/grammar/dom/InsertionPosition.java
@@ -2,7 +2,9 @@ package org.silnith.parser.html5.grammar.dom;
 
 import org.w3c.dom.Node;
 
-
+/**
+ * @see <a href="https://www.w3.org/TR/2014/REC-html5-20141028/syntax.html#appropriate-place-for-inserting-a-node">appropriate place for inserting a node</a>
+ */
 public interface InsertionPosition {
     
     Node getContainingNode();
---
+++ src/main/java/org/silnith/parser/html5/grammar/dom/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes related to finding the
+ * <a href="https://www.w3.org/TR/2014/REC-html5-20141028/syntax.html#appropriate-place-for-inserting-a-node">appropriate place for inserting a node</a>
+ * during parsing.
+ * 
+ * @see <a href="https://www.w3.org/TR/2014/REC-html5-20141028/syntax.html#creating-and-inserting-nodes">8.2.5.1 Creating and inserting nodes</a>
+ */
+package org.silnith.parser.html5.grammar.dom;
